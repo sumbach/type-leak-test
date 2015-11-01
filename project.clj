0ef9ed2e4e1d6bbd76756f8ed631443eb143adf7
Sam Umbach <samumbach@gmail.com>
2015-11-01 18:48:51 -0500

    Upgrade to latest clj/cljs/cljsbuild

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject type-leak "0.2.17-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2665"]]
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:id "test"
                         :source-paths ["src"]
