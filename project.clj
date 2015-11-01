0a259530610aca8849a3c71ef6c91f719c55502b
Sam Umbach <samumbach@gmail.com>
2015-11-01 18:50:27 -0500

    Unit tests demonstrating build warnings but correct runtime behavior

--- project.clj
+++ project.clj
@@ -2,11 +2,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [com.cemerick/clojurescript.test "0.3.3" :exclusions [org.clojure/clojure]]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:id "test"
                         :source-paths ["src"]
                         :compiler {:output-to "target/test/warning.js"
                                    :output-dir "target/test"
                                    :optimizations :advanced
-                                   :pretty-print true}}]})
+                                   :pretty-print true}}]
+              :test-commands {"test" ["phantomjs" :runner "target/test/warning.js"]}})
--- src/warning/core.cljs
+++ src/warning/core.cljs
@@ -1,10 +1,41 @@
-(ns warning.core)
+(ns warning.core
+  (:require cemerick.cljs.test)
+  (:require-macros [cemerick.cljs.test :refer [is deftest testing]]))
 
 (defprotocol Foo
   (foo [x]))
 
 (extend-protocol Foo
-  string
-  (foo [x] x))
+  nil
+  (foo [_] "nil")
 
-(foo "bar")
+  number
+  (foo [_] "number")
+
+  string
+  (foo [_] "string")
+
+  boolean
+  (foo [_] "boolean")
+
+  function
+  (foo [_] "function")
+
+  object
+  (foo [_] "object")
+
+  array
+  (foo [_] "array"))
+
+(deftest clojurescript-compiler-warnings
+  (is (= "nil"      (foo nil)))        ;; WARNING: Use of undeclared Var warning.core/clj-nil
+  (is (= "nil"      (foo js/undefined)))
+  (is (= "number"   (foo 0)))          ;; WARNING: Use of undeclared Var warning.core/number
+  (is (= "string"   (foo "bar")))      ;; WARNING: Use of undeclared Var warning.core/string
+  (is (= "boolean"  (foo true)))
+  (is (= "boolean"  (foo false)))
+  (is (= "function" (foo identity)))
+  (is (= "function" (foo (fn [x] x)))) ;; WARNING: Use of undeclared Var warning.core/function
+  (is (= "function" (foo #(%))))       ;; WARNING: Use of undeclared Var warning.core/function
+  (is (= "object"   (foo #js {})))     ;; WARNING: Use of undeclared Var warning.core/object
+  (is (= "array"    (foo #js []))))
